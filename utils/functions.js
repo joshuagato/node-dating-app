156f88e028a0d99210a52ce7ef9346468aefd665
Joshua Gato <joshuagato37@gmail.com>
2026-07-10 10:46:56 +0000

    Added util functions

---
+++ utils/functions.js
@@ -0,0 +1,66 @@
+const crypto = require('crypto');
+const { createSigner } = require('fast-jwt');
+
+exports.generateVerificationCode = digits => {
+    // Generate a secure random integer between 0 and 9999
+    const code = crypto.randomInt(0, 10000);
+    
+    // Ensure it is always 4 digits long by padding with leading zeros if needed
+    return code.toString().padStart(digits, '0');
+};
+
+exports.generateTokenForUserId = id => {
+    const key = process.env.JWT_KEY;
+    const signSync = createSigner({ key });
+    const token = signSync({ id });
+    return token;
+}
+
+exports.generateCookiesForToken = (res, token) => {
+    res.cookie('token', token, {
+        maxAge: (24 * 60 * 60 * 1000) * 30,     // 30 days,
+        httpOnly: false,                        // Prevents client-side JS access (XSS protection)
+        secure: false,                          // Forces cookie to be sent over HTTPS only
+        sameSite: 'strict'                      // Mitigates CSRF attacks
+    });
+}
+
+exports.organizeErrors = errorsArray => {
+    const errorsData = {};
+
+    errorsArray.forEach(error => {
+        if (Object.keys(errorsData).includes(error['path'])) {
+            // errorsData[error['path']].push(error['msg']);
+            errorsData[error['path']] = [...errorsData[error['path']], error['msg']];
+        } else {
+            errorsData[error['path']] = [error['msg']];
+        }
+    })
+
+    return errorsData;
+}
+
+exports.deleteUserFields = user => {
+    delete user.dataValues.id;
+    delete user.dataValues.createdAt;
+    delete user.dataValues.updatedAt;
+    delete user.dataValues.email_verified;
+    delete user.dataValues.verification_code;
+    return user;
+}
+
+exports.updateUserEmailVerificationAndExpiration = async (user, verificationCode) => {
+    user.email_verification_code = verificationCode;
+    user.email_verification_code_expiration = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await user.save();
+}
+
+exports.updateUserPasswordResetConfirmationAndExpiration = async (user, confirmationCode) => {
+    user.password_reset_code = confirmationCode;
+    user.password_reset_code_expiration = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await user.save();
+}
+
+exports.checkForVerificationCodeExpiry = email_verification_code_expiration => {
+    return Date.now() <= new Date(email_verification_code_expiration);
+}
