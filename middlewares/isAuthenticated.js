1a350aa4d3eb5a1cb7389abad7cbd1446fe7b522
Joshua Gato <joshuagato37@gmail.com>
2026-07-10 10:36:46 +0000

    Removed status code from isAuthenticated middleware

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -5,17 +5,17 @@ const isAuthenticated = async (req, res, next) => {
     let token = req.cookies.token || req.headers.authorization || req.headers.token;
     
     const message = 'Token not found.';
-    if (!token) return res.status(404).json({ success: false, message });
+    if (!token) return res.json({ success: false, message });
 
     if (token.includes('Bearer'))
         token = token.split(' ')[1];
 
     const decode = createDecoder();
     const userId = decode(token).id;
-    if (!userId) return res.status(404).json({ success: false, message });
+    if (!userId) return res.json({ success: false, message });
     
     const user = await User.findByPk(userId);
-    if (!user) return res.status(404).json({ success: false, message });
+    if (!user) return res.json({ success: false, message });
     
     delete user.dataValues.password;
 
