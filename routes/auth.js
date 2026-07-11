116cfdf7b906b8de0199d614e02f34b9435d1717
Joshua Gato <joshuagato37@gmail.com>
2026-07-11 02:21:09 +0000

    Added routes for password reset

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { login, signup, profile, verifyEmail, requestPasswordReset } = require('../controllers/user');
+const { login, signup, profile, verifyEmail, requestPasswordReset, confirmPasswordReset,
+        resetPassword, 
+    } = require('../controllers/user');
 const { validateEmail, validatePassword, validateConfirmPassword } = require('../validators');
 
 router.post('/login', validateEmail(), validatePassword(), login);
@@ -13,6 +15,11 @@ router.post('/signup', validateEmail(), validatePassword(), validateConfirmPassw
 
 router.patch('/verify-email', IsAuthenticated, verifyEmail);
 
-router.patch('/request-password-reset', IsAuthenticated, validateEmail(), requestPasswordReset);
+// router.patch('/request-password-reset', IsAuthenticated, validateEmail(), requestPasswordReset);
+router.patch('/request-password-reset', validateEmail(), requestPasswordReset);
+
+router.post('/confirm-password-reset', IsAuthenticated, confirmPasswordReset);
+
+router.post('/reset-password', validatePassword(), validateConfirmPassword(), IsAuthenticated, resetPassword);
 
 module.exports = { authRouter: router };
