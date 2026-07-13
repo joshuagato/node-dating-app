5654a0e533438d51b056916e1871cdf6cc989eab
Joshua Gato <joshuagato37@gmail.com>
2026-07-13 22:45:39 +0000

    Udated email verification attempt

--- models/EmailVerificationAttempt.js
+++ models/EmailVerificationAttempt.js
@@ -7,8 +7,8 @@ const EmailVerificationAttempt = postgresSequelize.define('EmailVerificationAtte
     {
         id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
         user_id: { type: DataTypes.UUID, references: { model: 'Users', key: 'id' } },
-        verification_code: { type: DataTypes.STRING, defaultValue: null },
-        verification_code_correct: { type: DataTypes.BOOLEAN, defaultValue: false },
+        verification_code_entered: { type: DataTypes.STRING, defaultValue: null },
+        verification_code_correct: { type: DataTypes.BOOLEAN, allowNull: true },
         verification_code_expired: { type: DataTypes.BOOLEAN, defaultValue: false },
         verification_channel: { type: DataTypes.ENUM, values: [VERIFICATION_CHANNEL.LOGIN, VERIFICATION_CHANNEL.SIGNUP] },
     },
