835a04062fd4364ca8d87be9e8ba8cb1b164727c
Joshua Gato <joshuagato37@gmail.com>
2026-07-13 22:40:08 +0000

    Added 24 hours before and after now to constants

--- utils/constants.js
+++ utils/constants.js
@@ -21,4 +21,9 @@ const GENDER = {
     EVERONE
 }
 
-module.exports = { VERIFICATION_CHANNEL, GENDER };
+
+const TWENTY_FOUR_HOURS_FROM_NOW = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+const TWENTY_FOUR_HOURS_BEFORE_NOW = new Date(Date.now() - 24 * 60 * 60 * 1000);
+
+module.exports = { VERIFICATION_CHANNEL, GENDER, TWENTY_FOUR_HOURS_FROM_NOW, TWENTY_FOUR_HOURS_BEFORE_NOW };
