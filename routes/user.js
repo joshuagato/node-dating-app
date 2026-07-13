41a7ae07826c5a64a17e2cd6679e6d47214d39d6
Joshua Gato <joshuagato37@gmail.com>
2026-07-13 23:14:28 +0000

    Updated route path and controller name

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { getProfile, setupProfile, } = require('../controllers/user');
+const { getProfile, setupBasicProfile, } = require('../controllers/user');
 const { validateName, validatePassword, validateConfirmPassword } = require('../validators');
 
 
 router.get('/profile', IsAuthenticated, getProfile);
 
-router.put('/profile', 
+router.put('/basic-profile', 
     validateName('first_name'), validateName('last_name'), 
-    IsAuthenticated, setupProfile);
+    IsAuthenticated, setupBasicProfile);
 
 module.exports = { userRouter: router };
