4ba8d0e3625542a5ab72fdfcaec79584657828a0
Joshua Gato <joshuagato37@gmail.com>
2026-08-04 01:27:50 +0000

    Added user routes

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { getProfile, setupBasicProfile, } = require('../controllers/user');
+const { getProfile, setupBasicProfile, setupAdvancedProfile, getPotentialMatchProfiles } = require('../controllers/user');
 const { validateName, validatePassword, validateConfirmPassword } = require('../validators');
+const { upload } = require('../utils/utils');
 
 
 router.get('/profile', IsAuthenticated, getProfile);
 
-router.put('/basic-profile', 
-    validateName('first_name'), validateName('last_name'), 
+router.put('/basic-profile',
+    validateName('first_name'), validateName('last_name'),
     IsAuthenticated, setupBasicProfile);
+
+router.put('/advanced-profile', IsAuthenticated, upload.array('images'), setupAdvancedProfile);
+
+router.get('/get-potential-match-profiles', IsAuthenticated, getPotentialMatchProfiles);
 
 module.exports = { userRouter: router };
