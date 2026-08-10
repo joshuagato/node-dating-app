803611e3d488ee85595df41bb911bcf141812fb4
Joshua Gato <joshuagato37@gmail.com>
2026-08-10 03:39:41 +0000

    Added encounter actions constants

--- utils/constants.js
+++ utils/constants.js
@@ -14,11 +14,16 @@ const WOMEN = 'women';
 const EVERONE = 'everyone';
 
 const GENDER = {
-    MAN,
-    MEN,
-    WOMAN,
-    WOMEN,
-    EVERONE
+    MAN, MEN, WOMAN, WOMEN, EVERONE
+}
+
+const LIKE = 'like';
+const DISLIKE = 'dislike';
+const SUPER_LIKE = 'super-like';
+const PASS = 'pass';
+
+const ENCOUNTER_ACTION = {
+    LIKE, DISLIKE, SUPER_LIKE, PASS
 }
 
 
@@ -26,4 +31,7 @@ const TWENTY_FOUR_HOURS_FROM_NOW = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
 const TWENTY_FOUR_HOURS_BEFORE_NOW = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
-module.exports = { VERIFICATION_CHANNEL, GENDER, TWENTY_FOUR_HOURS_FROM_NOW, TWENTY_FOUR_HOURS_BEFORE_NOW };
+module.exports = {
+    VERIFICATION_CHANNEL, GENDER, TWENTY_FOUR_HOURS_FROM_NOW, TWENTY_FOUR_HOURS_BEFORE_NOW,
+    ENCOUNTER_ACTION
+};
