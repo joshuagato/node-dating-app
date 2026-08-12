3a58912879a5e344cfb783848d8b490d57274578
Joshua Gato <joshuagato37@gmail.com>
2026-08-12 14:55:39 +0000

    Added get user likes

--- controllers/encounter.js
+++ controllers/encounter.js
@@ -1,11 +1,21 @@
 const { validationResult, matchedData } = require('express-validator');
 const { Sequelize, Op } = require('sequelize');
+const moment = require('moment');
 
 const { organizeErrors } = require('../utils/functions');
 
 const Encounter = require('../models/Encounter');
 const Match = require('../models/Match');
-const { ENCOUNTER_ACTION } = require('../utils/constants');
+const User = require('../models/User');
+const UserProfile = require('../models/UserProfile');
+const { ENCOUNTER_ACTION, MATCH_STATUS } = require('../utils/constants');
+const UserPicture = require('../models/UserPicture');
+
+User.hasMany(Encounter, { foreignKey: 'initiator_id', as: 'initiatedEncounters' });
+User.hasMany(Encounter, { foreignKey: 'recipient_id', as: 'receivedEncounters' });
+
+Encounter.belongsTo(User, { foreignKey: 'initiator_id', as: 'initiator' });
+Encounter.belongsTo(User, { foreignKey: 'recipient_id', as: 'recipient' });
 
 
 exports.likeUser = async (req, res) => {
@@ -41,6 +51,116 @@ exports.likeUser = async (req, res) => {
 
     success = true;
     res.send({ success, match });
+}
+
+exports.getUserLikes = async (req, res) => {
+    const { id: currentUserId } = req.user;
+
+    // 1. Get IDs of users that the current user has already liked
+    const usersAlreadyLikedByMe = await Encounter.findAll({
+        where: {
+            initiator_id: currentUserId,
+            action: ENCOUNTER_ACTION.LIKE
+        },
+        attributes: ['recipient_id'],
+        raw: true
+    }).then(results => results.map(row => row.recipient_id));
+
+    // 2. Fetch incoming likes excluding already reciprocated users
+    const incomingLikes = await Encounter.findAll({
+        attributes: [
+            [
+                Sequelize.literal(`
+                TRIM(
+                    CONCAT(
+                        "initiator"."first_name", 
+                        CASE 
+                            WHEN "initiator->profile"."last_name_on" = TRUE AND "initiator"."last_name" IS NOT NULL 
+                            THEN CONCAT(' ', "initiator"."last_name") 
+                            ELSE '' 
+                        END,
+                        CASE 
+                            WHEN "initiator->profile"."other_names_on" = TRUE AND "initiator"."other_names" IS NOT NULL 
+                            THEN CONCAT(' ', "initiator"."other_names") 
+                            ELSE '' 
+                        END
+                    )
+                )
+            `),
+                'name'
+            ],
+            ['createdAt', 'liked_at'],
+            'seen',
+            [
+                Sequelize.literal(`
+                DATE_PART('year', AGE(CURRENT_DATE, "initiator"."date_of_birth"))::integer
+            `),
+                'age'
+            ],
+            [
+                Sequelize.literal(`
+                (
+                    SELECT json_agg(
+                        json_build_object(
+                            'path', up."path",
+                            'position', up."position"
+                        ) ORDER BY up."position" ASC
+                    )
+                    FROM "UserPictures" up
+                    WHERE up."user_id" = "initiator"."id"
+                )
+            `),
+                'pictures'
+            ],
+            [
+                Sequelize.literal('"initiator"."id"'),
+                'user_id'
+            ],
+            [
+                Sequelize.literal('"initiator"."country"'),
+                'country'
+            ]
+        ],
+        where: {
+            recipient_id: currentUserId,
+            action: ENCOUNTER_ACTION.LIKE,
+            initiator_id: {
+                [Op.notIn]: usersAlreadyLikedByMe
+            }
+        },
+        include: [
+            {
+                model: User,
+                as: 'initiator',
+                attributes: [],
+                include: [
+                    {
+                        model: UserProfile,
+                        as: 'profile',
+                        attributes: []
+                    }
+                ]
+            }
+        ],
+        order: [['createdAt', 'DESC']],
+        raw: true
+    });
+
+    // Process the results to handle null pictures
+    const likesx = incomingLikes.map(like => ({
+        ...like,
+        pictures: like.pictures || [] // Ensure pictures is always an array
+    }));
+
+    const likes = incomingLikes.map(like => ({
+        ...like, liked_at: moment(like.liked_at, 'YYYYMMDD').fromNow()
+    }))
+
+    const unseen = likes.some(like => !like.seen);
+
+    let success = true;
+    res.send({ success, unseen, likes });
+
 }
 
 
