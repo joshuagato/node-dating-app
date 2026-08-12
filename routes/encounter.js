1292d71a13cebab81e8b544bfc1c7a2b72b34b7c
Joshua Gato <joshuagato37@gmail.com>
2026-08-12 23:46:08 +0000

    Added users disliked by me route

--- routes/encounter.js
+++ routes/encounter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { likeUser, dislikeUser, getUsersWhoLikeMe, getUsersWhoDisLikeMe } = require('../controllers/encounter');
+const { likeUser, dislikeUser, getUsersWhoLikeMe, getUsersWhoDisLikeMe, getUsersDisLikedByMe } = require('../controllers/encounter');
 const { validateName, validatePassword, validateConfirmPassword, validateLocation,
     validateCoordinates
 } = require('../validators');
@@ -15,6 +15,8 @@ router.post('/dislike-user', IsAuthenticated, dislikeUser);
 router.get('/users-who-like-me', IsAuthenticated, getUsersWhoLikeMe);
 
 router.get('/users-who-dislike-me', IsAuthenticated, getUsersWhoDisLikeMe);
+
+router.get('/users-disliked-by-me', IsAuthenticated, getUsersDisLikedByMe);
 
 
 module.exports = { encounterRouter: router };
