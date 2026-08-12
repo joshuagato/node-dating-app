9ee7f82fa417c928edd712f84ac1d762b9c7e521
Joshua Gato <joshuagato37@gmail.com>
2026-08-12 14:56:26 +0000

    Added users likes route

--- routes/encounter.js
+++ routes/encounter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { likeUser, dislikeUser } = require('../controllers/encounter');
+const { likeUser, dislikeUser, getUserLikes } = require('../controllers/encounter');
 const { validateName, validatePassword, validateConfirmPassword, validateLocation,
     validateCoordinates
 } = require('../validators');
@@ -11,6 +11,8 @@ const { validateName, validatePassword, validateConfirmPassword, validateLocatio
 router.post('/like-user', IsAuthenticated, likeUser);
 
 router.post('/dislike-user', IsAuthenticated, dislikeUser);
+
+router.get('/user-likes', IsAuthenticated, getUserLikes);
 
 
 module.exports = { encounterRouter: router };
