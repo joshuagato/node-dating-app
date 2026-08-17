f4fcd21e9b5471480572068a1d6d13668b34df63
Joshua Gato <joshuagato37@gmail.com>
2026-08-17 21:40:05 +0000

    Updated router paths and added chat route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { authRouter } = require('../routes/auth');
-const { userRouter } = require('../routes/user');
-const { encounterRouter } = require('../routes/encounter');
+const { authRouter } = require('./auth');
+const { userRouter } = require('./user');
+const { encounterRouter } = require('./encounter');
+const { chatRouter } = require('./chat');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/encounter', encounterRouter);
+router.use('/chat', chatRouter);
 
 exports.apiRouter = router;
