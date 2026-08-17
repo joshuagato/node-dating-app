fa4d01a2b34a700aa9d1f43a32d34ac3758e6e6c
Joshua Gato <joshuagato37@gmail.com>
2026-08-17 21:22:30 +0000

    Added message model

---
+++ models/Message.js
@@ -0,0 +1,48 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const { postgresSequelize } = require('../database/postgresql');
+const { MESSAGE_TYPE, MESSAGE_STATUS, MESSAGE_DIRECTION, CALL_TYPE } = require('../utils/constants');
+
+const Message = postgresSequelize.define('Message',
+    {
+        id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+        match_id: {
+            type: DataTypes.UUID, allowNull: false, onDelete: 'CASCADE', references: { model: 'Matches', key: 'id' }
+        },
+        chat_id: {
+            type: DataTypes.UUID, allowNull: false, onDelete: 'CASCADE', references: { model: 'Chats', key: 'id' }
+        },
+        sender_id: {
+            type: DataTypes.UUID, allowNull: false, onDelete: 'CASCADE', references: { model: 'Users', key: 'id' }
+        },
+        recipient_id: {
+            type: DataTypes.UUID, allowNull: false, onDelete: 'CASCADE', references: { model: 'Users', key: 'id' }
+        },
+        content: { type: DataTypes.TEXT, allowNull: true },
+        message_type: { type: DataTypes.ENUM, values: Object.values(MESSAGE_TYPE), defaultValue: MESSAGE_TYPE.TEXT },
+        // status: {
+        //     type: DataTypes.ENUM, allowNull: false, values: Object.values(MESSAGE_STATUS), defaultValue: MESSAGE_STATUS.SENT
+        // },
+        // direction: { type: DataTypes.ENUM, values: Object.values(MESSAGE_DIRECTION) },
+        reply_to_id: { type: DataTypes.UUID, allowNull: true, references: { model: 'Messages', key: 'id' } },
+        // forwarded_from_id: {
+        //     type: DataTypes.UUID, allowNull: true, references: { model: 'Messages', key: 'id' }
+        // },
+        sent_at: { type: DataTypes.DATE, allowNull: true },
+        delivered_at: { type: DataTypes.DATE, allowNull: true },
+        read_at: { type: DataTypes.DATE, allowNull: true },
+        // attachments: { type: DataTypes.JSONB, defaultValue: [], allowNull: true },
+        // metadata: { type: DataTypes.JSONB, defaultValue: {}, allowNull: true },
+        call_duration: { type: DataTypes.INTEGER, allowNull: true },
+        call_type: { type: DataTypes.ENUM, values: Object.values(CALL_TYPE), allowNull: true },
+        // poll_data: { type: DataTypes.JSONB, allowNull: true },
+        // location: { type: DataTypes.JSONB, allowNull: true },
+        reactions: { type: DataTypes.JSONB, defaultValue: {}, allowNull: true },
+        is_deleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+        deleted_for: { type: DataTypes.ARRAY(DataTypes.UUID), defaultValue: [] },
+        is_important: { type: DataTypes.BOOLEAN, defaultValue: false },
+        edited_at: { type: DataTypes.DATE, allowNull: true },
+        edit_count: { type: DataTypes.INTEGER, defaultValue: 0 }
+    },
+);
+
+module.exports = Message;
