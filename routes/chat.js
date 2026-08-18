5c030697f3cb4fb938370682c7490b329ec6e3d0
Joshua Gato <joshuagato37@gmail.com>
2026-08-18 23:47:03 +0000

    Added mark as read route

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { sendMessage, getChats, getChatMessages } = require('../controllers/chat');
+const { sendMessage, getChats, getChatMessages, markMessageAsSeen } = require('../controllers/chat');
 const { validateChatMessage } = require('../validators');
 
 router.post('/send-message', validateChatMessage(), IsAuthenticated, sendMessage);
@@ -10,5 +10,7 @@ router.post('/send-message', validateChatMessage(), IsAuthenticated, sendMessage
 router.get('/get-chats', IsAuthenticated, getChats);
 
 router.get('/get-chat-messages/:chat_id', IsAuthenticated, getChatMessages);
+
+router.patch('/mark-message-as-read/:message_id', IsAuthenticated, markMessageAsSeen);
 
 module.exports = { chatRouter: router };
