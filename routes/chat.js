d805f8ba9a526a60efc84bdd900c71dc883c52d7
Joshua Gato <joshuagato37@gmail.com>
2026-08-18 19:07:10 +0000

    Added get chat messages route

--- routes/chat.js
+++ routes/chat.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { sendMessage, getChats } = require('../controllers/chat');
+const { sendMessage, getChats, getChatMessages } = require('../controllers/chat');
 const { validateChatMessage } = require('../validators');
 
 router.post('/send-message', validateChatMessage(), IsAuthenticated, sendMessage);
 
 router.get('/get-chats', IsAuthenticated, getChats);
+
+router.get('/get-chat-messages/:chat_id', IsAuthenticated, getChatMessages);
 
 module.exports = { chatRouter: router };
