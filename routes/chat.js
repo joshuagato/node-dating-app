3d7922a56abc29f5e6f0abef3a1de415f8f93a6f
Joshua Gato <joshuagato37@gmail.com>
2026-08-20 20:13:13 +0000

    Added get-new-chats-count route

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
-const { sendMessage, getChats, getChatMessages, markMessageAsSeen } = require('../controllers/chat');
+const { sendMessage, getChats, getChatMessages, markMessageAsSeen, getNewChatsCount } = require('../controllers/chat');
 const { validateChatMessage } = require('../validators');
 
 router.post('/send-message', validateChatMessage(), IsAuthenticated, sendMessage);
@@ -12,5 +12,7 @@ router.get('/get-chats', IsAuthenticated, getChats);
 router.get('/get-chat-messages/:chat_id', IsAuthenticated, getChatMessages);
 
 router.patch('/mark-message-as-read/:message_id', IsAuthenticated, markMessageAsSeen);
+
+router.get('/get-new-chats-count', IsAuthenticated, getNewChatsCount);
 
 module.exports = { chatRouter: router };
