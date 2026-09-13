83a5ae1997133fe32b4bf93446e7fe7c6e5ccf26
Joshua Gato <joshuagato37@gmail.com>
2026-09-13 04:30:23 +0000

    Added get encounters profiles route to encounter routes

--- routes/encounter.js
+++ routes/encounter.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const { IsAuthenticated } = require('../middlewares/isAuthenticated');
 const { likeUser, dislikeUser, getUsersWhoLikeMe, getUsersWhoDisLikeMe,
-    getUsersDisLikedByMe, getNewLikesCount
+    getUsersDisLikedByMe, getNewLikesCount, getEncountersProfiles
 } = require('../controllers/encounter');
 const { validateName, validatePassword, validateConfirmPassword, validateLocation,
     validateCoordinates
 } = require('../validators');
 
+
+router.get('/get-encounters-profiles', IsAuthenticated, getEncountersProfiles);
 
 router.post('/like-user', IsAuthenticated, likeUser);
 
