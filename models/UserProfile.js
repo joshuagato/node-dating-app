830731e3eeaff43e72853d78d8f20e602d5a422c
Joshua Gato <joshuagato37@gmail.com>
2026-09-15 23:32:40 +0000

    Added new fields to user profile model

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -9,7 +9,19 @@ const UserProfile = postgresSequelize.define('UserProfile',
         last_name_on: { type: DataTypes.BOOLEAN },
         other_names_on: { type: DataTypes.BOOLEAN, defaultValue: false },
         gender_on: { type: DataTypes.BOOLEAN },
+
+        // Setup Phase Fields
+        bio: { type: DataTypes.TEXT, allowNull: true },
+        reason_on_app: { type: DataTypes.STRING, allowNull: true },
+        education: { type: DataTypes.STRING, allowNull: true },
+        relationship_status: { type: DataTypes.STRING, defaultValue: 'Single' },
+        height_cm: { type: DataTypes.INTEGER, allowNull: true },
+        smoking: { type: DataTypes.STRING, defaultValue: 'Never' },
+        drinking: { type: DataTypes.STRING, defaultValue: 'Socially' }
     },
+    {
+        tableName: 'UserProfiles'
+    }
 );
 
 module.exports = UserProfile;
