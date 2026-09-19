1bca4f94800b7cbe3c198ec6332cb15777d441d4
Joshua Gato <joshuagato37@gmail.com>
2026-09-19 01:21:22 +0000

    Added env variables for db connection

--- database/postgresql.js
+++ database/postgresql.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 const { color, log } = require('console-log-colors');
 
-const postgresSequelize = new Sequelize('dating', 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres'
+const db_host = process.env.DB_HOST;
+const db_name = process.env.DB_NAME;
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+
+const postgresSequelize = new Sequelize(db_name, db_username, db_password, {
+    host: db_host,
+    dialect: 'postgres'
 });
 
 const connectPostgreSql = async () => {
@@ -16,9 +21,9 @@ const connectPostgreSql = async () => {
 }
 
 (async () => {
-//   await postgresSequelize.sync({ force: true });
-  await postgresSequelize.sync({ alter: true });
-//   await postgresSequelize.sync();
+    //   await postgresSequelize.sync({ force: true });
+    await postgresSequelize.sync({ alter: true });
+    //   await postgresSequelize.sync();
 })();
 
 exports.connectPostgreSql = connectPostgreSql;
