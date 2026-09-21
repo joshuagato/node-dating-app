ac819b696a8a9caa78c3340499f020f0937dbb1b
Joshua Gato <joshuagato37@gmail.com>
2026-09-21 00:52:27 +0000

    Added premium routes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const { authRouter } = require('./auth');
 const { userRouter } = require('./user');
 const { encounterRouter } = require('./encounter');
 const { chatRouter } = require('./chat');
+const { premiumRouter } = require('./premium');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/encounter', encounterRouter);
 router.use('/chat', chatRouter);
+router.use('/premium', premiumRouter);
 
 exports.apiRouter = router;
