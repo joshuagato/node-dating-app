3d3971489490a059715f230f678e5fbfeda5d6ad
Joshua Gato <joshuagato37@gmail.com>
2026-09-24 20:16:48 +0000

    Fixed bug

--- utils/functions.js
+++ utils/functions.js
@@ -37,7 +37,7 @@ exports.generateCookiesForToken = (res, token) => {
     res.cookie('token', token, {
         maxAge: (24 * 60 * 60 * 1000) * 30,                 // 30 days,
         httpOnly: false,                                    // Prevents client-side JS access (XSS protection)
-        secure: false,                                      // Forces cookie to be sent over HTTPS only
+        secure: isProduction,                                      // Forces cookie to be sent over HTTPS only
         sameSite: isProduction ? 'none' : 'lax',         // Mitigates CSRF attacks
         partitioned: isProduction,
         path: '/'
@@ -48,7 +48,7 @@ exports.generateCookiesForCurrentUserId = (res, user_id) => {
     res.cookie('user_id', user_id, {
         maxAge: (24 * 60 * 60 * 1000) * 30,                 // 30 days,
         httpOnly: false,                                    // Prevents client-side JS access (XSS protection)
-        secure: false,                                      // Forces cookie to be sent over HTTPS only
+        secure: isProduction,                                      // Forces cookie to be sent over HTTPS only
         sameSite: isProduction ? 'none' : 'lax',         // Mitigates CSRF attacks
         partitioned: isProduction,
         path: '/'
