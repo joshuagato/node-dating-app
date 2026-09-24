0e172660eec236d7fa317fc2a5f35aed95d7ec62
Joshua Gato <joshuagato37@gmail.com>
2026-09-24 03:11:10 +0000

    Added feedback routes

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const { userRouter } = require('./user');
 const { encounterRouter } = require('./encounter');
 const { chatRouter } = require('./chat');
 const { premiumRouter } = require('./premium');
+const { feedbackRouter } = require('./feedback');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/encounter', encounterRouter);
 router.use('/chat', chatRouter);
 router.use('/premium', premiumRouter);
+router.use('/feedback', feedbackRouter);
 
 exports.apiRouter = router;
