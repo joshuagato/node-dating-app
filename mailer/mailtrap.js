fbb9666cba60a2603d25c509fb5109d09f958854
Joshua Gato <joshuagato37@gmail.com>
2026-09-26 00:51:40 +0000

    Included sanbox messaging

--- mailer/mailtrap.js
+++ mailer/mailtrap.js
@@ -2,8 +2,14 @@ const { MailtrapClient } = require("mailtrap");
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
+// const mailtrapClient = new MailtrapClient({
+//   token: TOKEN,
+// });
+
 const mailtrapClient = new MailtrapClient({
-  token: TOKEN,
+    token: TOKEN, // API Token from Sandbox Settings
+    sandbox: true,
+    testInboxId: 4754758,                      // Found in your Mailtrap Sandbox Inbox settings
 });
 
 module.exports = { mailtrapClient }
